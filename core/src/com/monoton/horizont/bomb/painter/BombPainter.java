40471bd216fb6ee09b19c998544eb963672d9475
Monoton <anatman@gmail.com>
2017-11-19 17:58:20 +0100

    Added simple central explosion

--- core/src/com/monoton/horizont/bomb/painter/BombPainter.java
+++ core/src/com/monoton/horizont/bomb/painter/BombPainter.java
@@ -2,13 +2,15 @@ package com.monoton.horizont.bomb.painter;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class BombPainter extends ApplicationAdapter {
+public class BombPainter extends ApplicationAdapter implements InputProcessor{
 	private OrthographicCamera camera;
 	private FPSLogger logger;
 
@@ -43,6 +45,8 @@ public class BombPainter extends ApplicationAdapter {
 
 		groundBody.createFixture(groundBox,0);
 
+		Gdx.input.setInputProcessor(this);
+
 
 
 
@@ -55,6 +59,43 @@ public class BombPainter extends ApplicationAdapter {
 		}
 
 	}
+
+    private float  DEGTORAD = 0.0174532925199432957f;
+	private void explosion() {
+		int numRays = 20;
+		float blastPower = 1000;
+		for (int i = 0; i < numRays; i++) {
+			float angle = (i / (float) numRays) * 360 * DEGTORAD;
+
+			Vector2 rayDir = new Vector2 (MathUtils.sin(angle), MathUtils.cos(angle));
+
+			BodyDef bd = new BodyDef();
+			bd.type = BodyDef.BodyType.DynamicBody;
+			bd.fixedRotation = true; // rotation not necessary
+			bd.bullet = true; // prevent tunneling at high speed
+			bd.linearDamping = 10; // drag due to moving through air
+			bd.gravityScale = 0; // ignore gravity
+			bd.position.set(width/2, height/2); // start at blast center
+//			bd.linearVelocity = blastPower * rayDir;
+			bd.linearVelocity.set(new Vector2(blastPower*rayDir.x, blastPower * rayDir.y));
+			Body body = world.createBody(bd);
+
+
+			CircleShape circleShape = new CircleShape();
+//			circleShape.m_radius = 0.05; // very small
+			circleShape.setRadius(0.05f);
+
+			FixtureDef fd = new FixtureDef();
+			fd.shape =circleShape;
+			fd.density = 60 / (float) numRays; // very high - shared across all particles
+			fd.friction = 0; // friction not necessary
+			fd.restitution = 0.99f; // high restitution to reflect off obstacles
+			fd.filter.groupIndex = -1; // particles should not collide with each other
+//			body -> CreateFixture( & fd);
+			body.createFixture(fd);
+		}
+	}
+
 
 	private void createCircleBody(float x, float y) {
 		BodyDef circleDef = new BodyDef();
@@ -90,6 +131,54 @@ public class BombPainter extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		world.dispose();
+
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return true;
+
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return true;
+
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		explosion();
+		return true;
+
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return true;
+
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return true;
+
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return true;
+
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return true;
 
 	}
 }
